7bca423c777d296de97b25219a799bd0b86a1286
fcorolla <enterprise.fjc@gmail.com>
2025-05-24 12:55:02 -0500

    Auto-sync update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import CustomBuilds from "./components/CustomBuilds";
 
 function App() {
   return (
-    <div>
-      <h1>QuantumForge UI is running!</h1>
+    <div className="p-6 bg-gray-800 min-h-screen text-white">
+      <h1 className="text-3xl font-bold">QuantumForge Loadout Optimizer</h1>
+      <CustomBuilds />
     </div>
   );
 }
